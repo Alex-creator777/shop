6cbd271e953deac0c5c7a5a1dedf9c8a4a54e76a
Алексей <randsx@yandex.ru>
2025-02-12 09:12:40 +0400

    clear comments

--- src/main/java/ru/leontev/shop/controllers/WarehouseController.java
+++ src/main/java/ru/leontev/shop/controllers/WarehouseController.java
@@ -2,15 +2,13 @@ package ru.leontev.shop.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
-import ru.leontev.shop.dto.response.StockItemResponseDto;
 import ru.leontev.shop.dto.response.WarehouseResponseDto;
-import ru.leontev.shop.service.StockItemService;
 import ru.leontev.shop.service.WarehouseService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/warehouse") // Базовый URL для всех методов контроллера
+@RequestMapping("/warehouse")
 public class WarehouseController {
 
     private final WarehouseService warehouseService;
